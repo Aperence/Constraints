9ff0741aca8ef3c43c60fa4cb1b2503f06488b15
Aperence <anthony.doeraene@hotmail.com>
2023-05-21 21:11:26 +0200

    solver

---
+++ solve.py
@@ -0,0 +1,46 @@
+from arc_consistency import *
+from constraints import *
+
+def mvr(domains):
+    idx = 0
+    size = len(domains[0])
+    for i in range(len(domains)):
+        domain = domains[i]
+        if len(domain) < size:
+            size = len(domain)
+            idx = i
+
+    return idx
+
+def backtrack(variables, domains, constraints):
+    """
+    Return true if assignment found, results are stored in variables
+    """
+    if (len(variables) == 0):
+        for constraint in constraints:
+            if not constraint.evaluate():
+                return False
+        return True
+    
+    restricted_domains = arc_consistency(variables, domains, constraints)
+    domains = restricted_domains
+
+    for domain in domains:
+        if (len(domain) == 0):
+            return False
+
+    var_idx = mvr(domains)
+    var = variables[var_idx]
+    rem_var = variables[:var_idx] + variables[var_idx+1:]
+
+    dom = domains[var_idx]
+    rem_dom = domains[:var_idx] + domains[var_idx+1:]
+
+    for value in dom:
+        var.assign(value)
+        res = backtrack(rem_var, rem_dom, constraints)
+        if res:
+            return True
+        var.unassign()
+
+    return False
--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 from constraints import *
 from arc_consistency import *
+from solve import backtrack
 
 x = Variable()
 y = Variable()
 z = Variable()
 
-constraints = [x >= 2*y + 1, z != y + 4]
+variables = [x, y, z]
+constraints = [x >= 3*y + 1, z != y + 5]
 domains = [{1,2,3,4,5}, {1,2,3,4}, {5,6}]
-print(arc_consistency([x, y, z], domains, constraints))
+res = backtrack(variables, domains, constraints)
+if (res):
+    print(F"x = {x.evaluate()}")
+    print(F"y = {y.evaluate()}")
+    print(F"z = {z.evaluate()}")
+else:
+    print("Impossible problem")
